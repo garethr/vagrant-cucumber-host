06799d8d4bccd070a4d056ef47f8584c4544ae5f
Gareth Rushgrove <gareth@morethanseven.net>
2014-03-07 17:52:50 +0000

    add a custom formatter to try and get output

--- lib/vagrant-cucumber-host/provisioner.rb
+++ lib/vagrant-cucumber-host/provisioner.rb
@@ -1,18 +1,16 @@
 require 'cucumber/cli/main'
-require 'gherkin/formatter/ansi_escapes'
 
 module VagrantPlugins
   module Cucumber
     class Provisioner < Vagrant.plugin('2', :provisioner)
-      include Gherkin::Formatter::AnsiEscapes
       def initialize(machine, config)
         super(machine, config)
 
         @features = config.features
       end
       def provisioner
+        require 'vagrant-cucumber-host/formatter'
         failure = Cucumber::Cli::Main.new([@features]).execute!
-        raise "Cucumber failed" if failure
       end
     end
   end
